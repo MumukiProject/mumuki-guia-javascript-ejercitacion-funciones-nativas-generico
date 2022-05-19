0716de0bba33b0ebbfa3bebdb80253cae2a5a573
mayra@mumuki.org <mayra@mumuki.org>
2022-05-19 17:38:50 +0000

    Mumuki Export on 2022-05-19 17:38:50 +0000

--- 00008_Capitalizar/description.md
+++ 00008_Capitalizar/description.md
@@ -1,8 +1,8 @@
 > Definí una función `capitalizar` que reciba como argumento un string `str` y devuelva el mismo string con la primera letra en mayúscula
 >
 ```javascript
-ム capitalizar('lovelace')
-'Lovelace'
+ム capitalizar('café')
+'Café'
 ム capitalizar('había una vez...')
 'Había una vez...'
 ```
--- 00008_Capitalizar/test..js
+++ 00008_Capitalizar/test..js
@@ -1,6 +1,6 @@
-it("Si hacemos capitalizar('lovelace') debería retornar 'Lovelace'", function() {
-let resultado = capitalizar('lovelace')
-assert.equal(resultado, 'Lovelace',`capitalizar('lovelace') debería retornar 'Lovelace' pero fue ${resultado}`);
+it("Si hacemos capitalizar('café') debería retornar 'Café'", function() {
+let resultado = capitalizar('café')
+assert.equal(resultado, 'Café',`capitalizar('café') debería retornar 'Café' pero fue ${resultado}`);
 })
 
 it("Si hacemos capitalizar('tereshkova') debería retornar 'Tereshkova'", function() {

a0dc59effa84f4c070b9d260f6ef245bb704bbf2
mayra@mumuki.org <mayra@mumuki.org>
2022-05-19 16:37:56 +0000

    Mumuki Export on 2022-05-19 16:37:56 +0000

--- 00002_Es el último caracter/description.md
+++ 00002_Es el último caracter/description.md
@@ -1,8 +1,8 @@
 > Definí una función `esElUltimoCaracter` que reciba como argumentos una `palabra` y un `caracter` y nos indique si la `palabra` termina con el `caracter`.
 >
 ```javascript
-ム igualLongitud('lovelace', 'e')
+ム igualLongitud('internet', 't')
 true
-ム igualLongitud('lovelace', 'f')
+ム igualLongitud('internet', 'f')
 false
 ```
--- 00002_Es el último caracter/test..js
+++ 00002_Es el último caracter/test..js
@@ -1,9 +1,9 @@
-it("Si hacemos esElUltimoCaracter('lovelace', 'e') debería retornar true", function() {
-  let resultado = esElUltimoCaracter('lovelace', 'e')
-  assert.equal(resultado, true,`esElUltimoCaracter ('lovelace','e') debería retornar true pero fue ${resultado}`);
+internetit("Si hacemos esElUltimoCaracter('internet', 't') debería retornar true", function() {
+  let resultado = esElUltimoCaracter('internet', 't')
+  assert.equal(resultado, true,`esElUltimoCaracter ('internet','t') debería retornar true pero fue ${resultado}`);
 })
 
-it("Si hacemos esElUltimoCaracter('lovelace', 'f') debería retornar false", function() {
-  let resultado = esElUltimoCaracter('lovelace', 'f')
-  assert.equal(resultado, false,`esElUltimoCaracter ('lovelace','f') debería retornar false pero fue ${resultado}`);
+it("Si hacemos esElUltimoCaracter('internet', 'f') debería retornar false", function() {
+  let resultado = esElUltimoCaracter('internet', 'f')
+  assert.equal(resultado, false,`esElUltimoCaracter ('internet','f') debería retornar false pero fue ${resultado}`);
 })

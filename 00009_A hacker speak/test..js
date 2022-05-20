8d6f9f2ee94121a598eb9ba02e78581a39107e8e
mayra@mumuki.org <mayra@mumuki.org>
2022-05-20 19:56:40 +0000

    Mumuki Export on 2022-05-20 19:56:40 +0000

--- 00009_A hacker speak/test..js
+++ 00009_A hacker speak/test..js
@@ -3,9 +3,9 @@ let resultado = aHackerSpeak('javascript')
 assert.equal(resultado, 'j4v45cr1pt',`aHackerSpeak('javascript') debería retornar 'j4v45cr1pt' pero fue ${resultado}`);
 })
 
-it("Si hacemos aHackerSpeak('soy una hacker') debería retornar '50y un4 h4ck3r'", function() {
-let resultado = aHackerSpeak('soy una hacker')
-assert.equal(resultado, '50y un4 h4ck3r',`aHackerSpeak('soy una hacker') debería retornar '50y un4 h4ck3r' pero fue ${resultado}`);
+it("Si hacemos aHackerSpeak('soy un hacker') debería retornar '50y un h4ck3r'", function() {
+let resultado = aHackerSpeak('soy un hacker')
+assert.equal(resultado, '50y un h4ck3r',`aHackerSpeak('soy un hacker') debería retornar '50y un h4ck3r' pero fue ${resultado}`);
 })
 
 it("Si hacemos aHackerSpeak('ADA LOVELACE') debería retornar '4D4 L0V3L4C3'", function() {
